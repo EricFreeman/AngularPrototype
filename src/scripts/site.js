5dc4aeafad8c88a0e09bff94ea4bfe41aadc7000
Eric Freeman <clownhunter@gmail.com>
2015-05-12 20:10:30 -0600

    Added directive for product selector.

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -1,5 +1,24 @@
 var productFlowModule = angular.module('productFlowModule', []);
 
-productFlowModule.controller("StepController", ['$scope', function($scope) {
-	$scope.stepName = "Find Your Branch";
-}]);
+productFlowModule
+	.controller('StepController', ['$scope', function($scope) {
+		$scope.stepNumber = 1;
+		$scope.stepName = 'Find Your Branch';
+		$scope.stepHeader = 'Find Your Brand of Contacts';
+
+		$scope.chooseProduct = function() {
+			console.log('product chosen');
+		}
+	}])
+	.controller('ProductSelectorController', ['$scope', function($scope) {
+
+	}])
+	.directive('productSelector', function() {
+		return {
+   			restrict: 'E',
+   			scope: {
+   				callback: '&selectProduct'
+   			},
+			template: '<a href="#" ng-click="callback()">Buy Me</a>'
+		}
+	});
