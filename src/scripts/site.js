2443a6ec5164e3e712c63d04e01ddd4db95b97cb
Eric Freeman <clownhunter@gmail.com>
2015-05-13 21:24:44 -0600

    Adding more work for multi step support.

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -3,15 +3,41 @@ var productFlowModule = angular.module('productFlowModule', []);
 productFlowModule
 	.controller('StepController', ['$scope', function($scope) {
 		$scope.stepNumber = 1;
-		$scope.stepName = 'Find Your Branch';
-		$scope.stepHeader = 'Find Your Brand of Contacts';
+		$scope.stepName = function() {
+			return $scope.stepMap[$scope.stepNumber].stepName;
+		}
+
+		$scope.stepMap = [
+			{ stepNumber: 1, stepName: 'Find My Brand', stepHeader: 'Find Your Brand of Contacts' },
+			{ stepNumber: 2, stepName: 'Eye Options', stepHeader: 'Do you wear the same brand of contacts in both eyes?' },
+			{ stepNumber: 3, stepName: 'Enter Prescription', stepHeader: 'Enter your Rx for your Left Eye' },
+			{ stepNumber: 4, stepName: 'Patient Name', stepHeader: 'Who are these for?' },
+			{ stepNumber: 5, stepName: 'Search Doctor', stepHeader: 'Which doctor prescribed these contact lenses?' },
+			{ stepNumber: 6, stepName: 'Select Doctor', stepHeader: 'These are the doctors we found:' },
+			{ stepNumber: 7, stepName: 'Shipping Info', stepHeader: 'Where would you like to ship them?' },
+			{ stepNumber: 8, stepName: 'Billing Info', stepHeader: 'How would you like to pay for them today?' },
+		];
 
 		$scope.chooseProduct = function() {
 			console.log('product chosen');
+			$scope.incrementStep(1);
 		}
+
+		$scope.chooseEyeOptions = function() {
+			console.log('eye options chosen');
+			$scope.incrementStep(2);
+		}
+
+		$scope.incrementStep = function(currentStepNumber) {
+			if(currentStepNumber == $scope.stepNumber)
+			{
+				$scope.stepNumber++;
+			}
+		}
+
 	}])
 	.controller('ProductSelectorController', ['$scope', function($scope) {
-
+		$scope.brands = ['Acuvue', 'Air Optix', 'Avaira', 'Biofinity', 'Biomedics', 'DAILIES', 'Extreme H2O', 'FreshLook', 'Proclear', 'PureVision', 'SoftLens'];
 	}])
 	.directive('productSelector', function() {
 		return {
@@ -19,6 +45,20 @@ productFlowModule
    			scope: {
    				callback: '&selectProduct'
    			},
-			template: '<a href="#" ng-click="callback()">Buy Me</a>'
+			template: 
+				'<div class="well well-lg">' +
+					'<h3>Find Your Brand of Contacts</h3>' +
+					'<div ng-controller="ProductSelectorController">' +
+						'<span ng-repeat="brand in brands">' +
+							'<button type="button" class="btn btn-default" ng-click="callback()">{{ brand }}</button>' +
+						'</span>' +
+					'</div>' +
+				'</div>'
+		}
+	})
+	.directive('eyeOptions', function() {
+		return {
+			template:
+				'<div><p>test test</p></span><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />'
 		}
 	});
